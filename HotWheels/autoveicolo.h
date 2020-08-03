78b51f778ab2c983a43705ac3bb0877a944e87fa
Alfredo <graziano.alfredo34@yahoo.com>
2020-08-03 23:04:55 +0200

    Aggiunto campi dati e metodi su autoveicoli.h

--- HotWheels/autoveicolo.h
+++ HotWheels/autoveicolo.h
@@ -1,20 +1,33 @@
 #ifndef AUTOVEICOLO_H
 #define AUTOVEICOLO_H
-#include <iostream>
-
+#include<QDate>
+#include <string>
 using std::string;
 
-class autoveicolo
-{
-    private:
+class autoveicolo {
+private:
+    string marca;
+    string modello;
+    QDate annoImmatricolazione;
     int potenza;
     string classeambientale;
+    int peso;
+    int numeroAssi;
+
 
 public:
     autoveicolo(int = 0, string = "");
     ~autoveicolo() = default;
+    string getMarca() const;
+    string getModello() const;
+    QDate getData() const;
+    int getGiorno() const;
+    int getMese() const;
+    int getAnno() const;
     int getPotenza() const;
     string getClasseambientale() const;
+    int getPeso() const;
+    int getNumeroAssi() const;
 };
 
 #endif // AUTOVEICOLO_H
